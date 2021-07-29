c32d9f5124414cfaa1855f18776c7b1a379d1be1
kevinmgamboa <ing.kevin@hotmail.com>
2021-07-29 17:26:46 +0100

    Adding different takeoff modalities but doesn't work quite well

--- examples/takeoff_simple.py
+++ examples/takeoff_simple.py
@@ -1,16 +1,33 @@
 from djitellopy import Tello
-
+from time import sleep
+#%%
 # ------------------------------------------------------------------------------------
 #                                    Drone Setup
 # ------------------------------------------------------------------------------------
 tello = Tello()
 tello.connect()
+print(tello.get_battery())
+#%%
 # ------------------------------------------------------------------------------------
-#                                       Main
+#                                   Takeoff and Land
 # ------------------------------------------------------------------------------------
 # taking off the drone
 tello.takeoff()
-# rotating drone
-tello.rotate_counter_clockwise(90)
+sleep(3)
+print(f'taking off the ground \nBatery: {tello.get_battery()}')
+# landing drone
+tello.land()
+#%%
+# ------------------------------------------------------------------------------------
+#                                  Takeoff, rotate and Land
+# ------------------------------------------------------------------------------------
+# taking off the drone
+tello.takeoff()
+sleep(5)
+print(f'taking off the ground \nBatery: {tello.get_battery()}')
+for i in range(4):
+    # rotating drone
+    tello.rotate_counter_clockwise(90)
+    sleep(5)
 # landing drone
 tello.land()
